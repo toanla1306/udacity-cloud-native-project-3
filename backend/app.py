5072b72e1777a02da53900efffcc289bdb84b2d3
toan la <ldtoan1306@gmail.com>
2022-09-19 10:58:29 +0700

    update api backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,42 @@
 from flask import Flask, render_template, request, jsonify
 
+import os
 import pymongo
+import logging
 from flask_pymongo import PyMongo
 
 from prometheus_flask_exporter import PrometheusMetrics
 
+from jaeger_client import Config
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+# Configure Jaeger tracer
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+        validate=True
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
 app = Flask(__name__)
+tracer = init_tracer('backend')
+FlaskInstrumentor().instrument_app(app)
+
 metrics = PrometheusMetrics(app)
-metrics.info('app_info', 'Application info', version='1.0.3')
+metrics.info('backend', 'Backend Application info', version='1.0.3')
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
@@ -30,7 +59,8 @@ historgram_status_path = metrics.histogram('requests_by_status_and_path', 'Reque
 @record_requests_by_status
 @common_counter
 def homepage():
-    return "Hello World"
+    with tracer.start_active_span('home-page'):
+        return "Hello World"
 
 
 @app.route("/api")
@@ -38,8 +68,9 @@ def homepage():
 @common_counter
 @historgram_status_path
 def my_api():
-    answer = "something"
-    return jsonify(repsonse=answer)
+    with tracer.start_span('my-api'):
+        answer = "something"
+        return jsonify(repsonse=answer)
 
 
 @app.route("/star", methods=["POST"])
@@ -47,13 +78,14 @@ def my_api():
 @common_counter
 @historgram_status_path
 def add_star():
-    star = mongo.db.stars
-    name = request.json["name"]
-    distance = request.json["distance"]
-    star_id = star.insert({"name": name, "distance": distance})
-    new_star = star.find_one({"_id": star_id})
-    output = {"name": new_star["name"], "distance": new_star["distance"]}
-    return jsonify({"result": output})
+    with tracer.start_span('add star'):
+        star = mongo.db.stars
+        name = request.json["name"]
+        distance = request.json["distance"]
+        star_id = star.insert({"name": name, "distance": distance})
+        new_star = star.find_one({"_id": star_id})
+        output = {"name": new_star["name"], "distance": new_star["distance"]}
+        return jsonify({"result": output})
 
 
 if __name__ == "__main__":
